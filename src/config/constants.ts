bc2647cd9ff36744bbc462995efc292525424b51
Fabs <gianfranco.aguirre.cruz@gmail.com>
2024-06-13 17:48:07 -0500

    ✨ feat: Add Header component with navigation links

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -13,3 +13,23 @@ export const profileData = {
     github: "https://github.com/fabaguirre",
   },
 };
+
+export const SECTIONS = [
+  {
+    id: "experience",
+    title: "Experience",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+    href: profileData.mailto,
+  },
+];
