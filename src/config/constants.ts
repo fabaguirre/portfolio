e7f379c86af55802354127d69133067cf88cd476
Fabs <gianfranco.aguirre.cruz@gmail.com>
2024-06-13 01:31:57 -0500

    🔧 feat: Add profileData constant with personal information

---
+++ src/config/constants.ts
@@ -0,0 +1,14 @@
+export const profileData = {
+  names: {
+    firstName: "Fabrizio",
+    callName: "Fab",
+    socialNickname: "fabos",
+    lastName: "Aguirre",
+  },
+  email: "faguirrecruz@hotmail.com",
+  titles: ["Software Engineer", "Frontend Developer", "Full Stack Developer"],
+  socialMedia: {
+    linkedin: "https://www.linkedin.com/in/fabaguirre",
+    github: "https://github.com/fabaguirre",
+  },
+};
