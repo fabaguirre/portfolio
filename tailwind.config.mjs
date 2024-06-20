a398d95ea4e148d92446d978bb85f33c28cd5868
Fab Aguirre <gianfranco.aguirre.cruz@gmail.com>
2024-06-20 00:52:37 -0500

    🔧 refactor: Update tailwind.config.mjs to include themeVariants plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import { themeVariants } from "tailwindcss-theme-variants";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    themeVariants({
+      themes: {
+        light: {
+          selector: ":not(.dark)",
+        },
+      },
+    }),
+  ],
   darkMode: "selector",
 };
