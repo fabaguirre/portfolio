a0585bf315f80c8782ef2e5279d67a8ebc94d191
Fabs <gianfranco.aguirre.cruz@gmail.com>
2024-08-27 17:47:03 -0500

    💄 refactor: Update global text and background colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,26 @@ import { themeVariants } from "tailwindcss-theme-variants";
 export default {
   content: ["./src/**/*.{astro,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: {
+          100: "#FFFAF0",
+          200: "#FEEBC8",
+          300: "#FBD38D",
+          400: "#F6AD55",
+          500: "#ED8936",
+          600: "#DD6B20",
+          700: "#C05621",
+          800: "#9C4221",
+          900: "#7B341E",
+        },
+        // light: '#F8F8FF', // Ghost White
+        light: '#FAF9F6', // Off White
+        dark: '#100D08', // Black Chocolate
+        // dark: '#212121', // Midnight Blue
+        // dark: '#11181C', // Arsenic
+      }
+    },
   },
   plugins: [
     themeVariants({
